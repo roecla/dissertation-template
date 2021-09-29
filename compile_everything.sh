efb32a87060ef736693343f61694e5967ba42996
Klara Roehrl <klara.roehrl@gmail.com>
2021-09-29 13:58:13 +0200

    Improve folder names.

--- compile_everything.sh
+++ compile_everything.sh
@@ -7,13 +7,13 @@ pdflatex -output-directory=bld/CV/ CV/CV.tex &&
 cp bld/CV/CV.pdf CV.pdf && 
 
 # Compile the Presentation
-pdflatex -output-directory=bld/presentation/ Presentation/presentation.tex && 
+pdflatex -output-directory=bld/presentation/ presentation/presentation.tex && 
 biber bld/presentation/presentation && 
-pdflatex -output-directory=bld/presentation/ Presentation/presentation.tex && 
+pdflatex -output-directory=bld/presentation/ presentation/presentation.tex && 
 cp bld/presentation/presentation.pdf presentation.pdf && 
 
 # Compile the Dissertation
-pdflatex -output-directory=bld/dissertation/ Dissertation/dissertation.tex && 
+pdflatex -output-directory=bld/dissertation/ dissertation/dissertation.tex && 
 biber bld/dissertation/dissertation && 
-pdflatex -output-directory=bld/dissertation Dissertation/dissertation.tex && 
+pdflatex -output-directory=bld/dissertation dissertation/dissertation.tex && 
 cp bld/dissertation/dissertation.pdf dissertation.pdf 
